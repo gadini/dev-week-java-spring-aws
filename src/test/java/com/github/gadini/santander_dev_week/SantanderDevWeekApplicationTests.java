be037ac1cc92169ccaebcd36514ead2e18bd7861
gustur0 <alvesrochagadini@gmail.com>
2024-10-08 10:45:31 -0300

    Initial

---
+++ src/test/java/com/github/gadini/santander_dev_week/SantanderDevWeekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.gadini.santander_dev_week;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDevWeekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
